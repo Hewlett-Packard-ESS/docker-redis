a1d91de6dba4c5b579787150097dbf3237d9eba3
Karl Stoney <karl@fedora.emea.hpqcorp.net>
2015-01-08 12:02:36 +0000

    Further bug fixes

--- cookbooks/redis/recipes/default.rb
+++ cookbooks/redis/recipes/default.rb
@@ -1,3 +1,15 @@
+dirs=%w(/storage/redis /var/log/redis)
+
+dirs.each do |dir|
+  directory "#{dir}" do
+    owner 'docker'
+    group 'docker'
+    action :create
+    recursive true
+  end
+end
+
+
 template '/storage/redis/redis-sentinel.conf' do
   source 'sentinel.conf.erb'
   variables ({ :confvars => { :redis => ENV['HOSTNAME'] } })
